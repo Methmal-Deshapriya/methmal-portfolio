e6fa43d41fe9806a617b42bf544cf57961230d4d
Methmal-Deshapriya <methmaldeshapriya2002@gmail.com>
2024-11-17 00:54:35 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,34 +8,7 @@ export const HERO_CONTENT = `A Developer and Designer with years of experience, 
 export const ABOUT_TEXT = `I’m an undergraduate at the University of Sri Jayewardenepura and a data science and machine learning enthusiast, currently working on development and design projects. I’ve made some progress in my journey, with much more to learn and achieve. My goal is to strengthen my skills in data analytics, data visualization, machine learning, and data engineering to make a meaningful impact in the data science industry.`;
 
 export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+ 
 ];
 
 export const PROJECTS = [
