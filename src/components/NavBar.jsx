8ced75bf129d91cb3ade5fe0188a72348194d0cf
Methmal-Deshapriya <methmaldeshapriya2002@gmail.com>
2024-11-17 00:27:36 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import React from "react";
+import logo from "../assets/des.png";
+const NavBar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="text-3xl font-normal text-neutral">Md</h1>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/methmal-deshapriya-988b3b2a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "
+          target="_blank"
+        >
+          <FaLinkedin className="cursor-pointer" />
+        </a>
+        <a href="https://github.com/Methmal-Deshapriya" target="_blank">
+          <FaGithub className="cursor-pointer" />
+        </a>
+
+        <a
+          href="https://www.facebook.com/profile.php?id=100081456306006&mibextid=ZbWKwL"
+          target="_blank"
+        >
+          <FaFacebook className="cursor-pointer" />
+        </a>
+
+        <a href="">
+          <FaInstagram className="cursor-pointer" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import project1 from "../assets/projects/mproject-1.jpg";
+import project2 from "../assets/projects/mproject-2.png";
+import project3 from "../assets/projects/mproject-4.jpg";
+import project4 from "../assets/projects/mproject-3.jpg";
+
+export const HERO_CONTENT = `A Developer and Designer with years of experience, and a data science and machine learning enthusiast. I enjoy building websites and creating 3D models as a hobby to showcase my design skills. My expertise includes React.js, HTML, CSS, and other front-end technologies, along with programming knowledge in Java, Python, C and C#.`;
+
+export const ABOUT_TEXT = `I’m an undergraduate at the University of Sri Jayewardenepura and a data science and machine learning enthusiast, currently working on development and design projects. I’ve made some progress in my journey, with much more to learn and achieve. My goal is to strengthen my skills in data analytics, data visualization, machine learning, and data engineering to make a meaningful impact in the data science industry.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "FoodAPP",
+    image: project1,
+    description:
+      "A fully functional application for search food and recipies with features like search food, view recipies.",
+    technologies: ["HTML", "CSS", "React", "Node.js"],
+  },
+  {
+    title: "Todo App",
+    image: project2,
+    description:
+      "An application for managing todos and send notifications, with features such as task listing, task tracking.",
+    technologies: ["HTML", "CSS", "React", "Node.js"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "FramerMotion"],
+  },
+  {
+    title: "Realistic 3D models",
+    image: project4,
+    description: "3D modeled cars,animations",
+    technologies: ["Blender", "Cycles"],
+  },
+];
+
+export const CONTACT = {
+  address: "No34 Boralugoda,Kosgama,Sri Lanka ",
+  phoneNo: "+94757451258 ",
+  email: "methmaldeshapriya2002@gmail.com",
+};
